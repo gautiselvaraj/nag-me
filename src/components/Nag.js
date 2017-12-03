d150203bfbfa9e7123f43a0a1b1eb015167e3bb2
Gauti <me@gauti.info>
2017-12-03 21:36:45 +1300

    Introduce react motion

--- src/components/Nag.js
+++ src/components/Nag.js
@@ -2,25 +2,18 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import humanizeDuration from 'humanize-duration';
+import {StaggeredMotion, spring} from 'react-motion';
 import {humanizeNextNag} from '../utils/time';
 import Button from './Button';
 import Icon from './Icon';
 
-const NagWrap = styled.li`
+const NagWrap = styled.div`
   background-color: #ffffff;
   border: 2px solid transparent;
   border-radius: 4px;
   box-shadow: 0 0 2px rgba(0, 0, 0, .25);
-  cursor: pointer;
-  margin-bottom: .5rem;
   padding: 5px;
-  position: relative;
-  transition: transform .35s;
-  ${props => props.active ? 'transform: translateX(-150px)' : ''};
-
-  &:last-child {
-    margin-bottom: 0;
-  }
+  will-change: transform;
 `;
 
 const NagHeading = styled.h3`
@@ -42,17 +35,28 @@ const NagRepeat = styled.p`
   margin-top: 5px;
 `;
 
-const NagLinks = styled.div`
+const NagEditLink = styled.div`
   position: absolute;
-  right: -135px;
+  right: -45px;
   top: 50%;
-  transform: translateY(-50%);
-  width: 120px;
-
-  button {
-    margin-left: .5rem;
-  }
+  will-change: transform;
 `;
+
+const NagDeleteLink = styled.div`
+  position: absolute;
+  right: -85px;
+  top: 50%;
+  will-change: transform;
+`;
+
+const NagStateLink = styled.div`
+  position: absolute;
+  right: -125px;
+  top: 50%;
+  will-change: transform;
+`;
+
+const nagSpring = {stiffness: 250, damping: 22};
 
 export default class Nag extends Component {
   static propTypes = {
@@ -64,6 +68,21 @@ export default class Nag extends Component {
   updateTimestamp = () => {
     this.setState({nowTimestamp: Date.now()});
   }
+
+  getInterpolatedStyles = prevInterpolatedStyles => {
+    const endValue = prevInterpolatedStyles.map((_, i) => {
+      if(this.state.active) {
+        return i === 0 ?
+          {t: spring(-135, nagSpring)} :
+          {t: spring(prevInterpolatedStyles[i - 1].t, nagSpring)};
+      } else {
+        return i === 3 ?
+          {t: spring(0, nagSpring)} :
+          {t: spring(prevInterpolatedStyles[i + 1].t, nagSpring)};
+        }
+    });
+    return endValue;
+  };
 
   handleClick = () => {
     this.setState({active: !this.state.active})
@@ -79,34 +98,70 @@ export default class Nag extends Component {
 
   render() {
     const {nag} = this.props;
-    const {nowTimestamp, active} = this.state;
+    const {nowTimestamp} = this.state;
 
     return (
-      <NagWrap active={active} tabindex="0" onClick={this.handleClick}>
-        <NagHeading>{nag.title}</NagHeading>
-        <NagTimer>in {humanizeNextNag(nag.nextNag - nowTimestamp)}</NagTimer>
-        <NagRepeat>
-          {!!nag.repeats &&
-            <span>and repeat every {humanizeDuration(nag.repeats)}</span>
-          }
-        </NagRepeat>
-        <NagLinks>
-          <Button circle title={`Edit ${nag.title} Nag`}>
-            <Icon edit />
-          </Button>
-          <Button circle title={`Delete ${nag.title} Nag`}>
-            <Icon delete />
-          </Button>
-          {nag.paused ?
-            <Button circle title={`Resume ${nag.title} Nag`}>
-              <Icon resume />
-            </Button> :
-            <Button circle title={`Pause ${nag.title} Nag`}>
-              <Icon pause />
-            </Button>
-          }
-        </NagLinks>
-      </NagWrap>
+      <StaggeredMotion
+        defaultStyles={Array.from(Array(4)).map(i => ({t: 0}))}
+        styles={this.getInterpolatedStyles}>
+        {interpolatingStyles =>
+          <div onClick={this.handleClick}>
+            {interpolatingStyles.map(({t}, i) => (
+              <div key={i}>
+                {i === 0 &&
+                  <NagWrap style={{
+                    WebkitTransform: `translateX(${t}px)`,
+                    transform: `translateX(${t}px)`
+                  }}>
+                    <NagHeading>{nag.title}</NagHeading>
+                    <NagTimer>in {humanizeNextNag(nag.nextNag - nowTimestamp)}</NagTimer>
+                    <NagRepeat>
+                      {!!nag.repeats &&
+                        <span>and repeat every {humanizeDuration(nag.repeats)}</span>
+                      }
+                    </NagRepeat>
+                  </NagWrap>
+                }
+                {i === 1 &&
+                  <NagEditLink style={{
+                    WebkitTransform: `translate(${t}px, -50%)`,
+                    transform: `translate(${t}px, -50%)`
+                  }}>
+                    <Button circle title={`Edit ${nag.title} Nag`}>
+                      <Icon edit />
+                    </Button>
+                  </NagEditLink>
+                }
+                {i === 2 &&
+                  <NagDeleteLink style={{
+                    WebkitTransform: `translate(${t}px, -50%)`,
+                    transform: `translate(${t}px, -50%)`
+                  }}>
+                    <Button circle title={`Delete ${nag.title} Nag`}>
+                      <Icon delete />
+                    </Button>
+                  </NagDeleteLink>
+                }
+                {i === 3 &&
+                  <NagStateLink style={{
+                    WebkitTransform: `translate(${t}px, -50%)`,
+                    transform: `translate(${t}px, -50%)`
+                  }}>
+                    {nag.paused ?
+                      <Button circle title={`Resume ${nag.title} Nag`}>
+                        <Icon resume />
+                      </Button> :
+                      <Button circle title={`Pause ${nag.title} Nag`}>
+                        <Icon pause />
+                      </Button>
+                    }
+                  </NagStateLink>
+                }
+              </div>
+            ))}
+          </div>
+        }
+      </StaggeredMotion>
     )
   }
 }
--- src/components/Nags.js
+++ src/components/Nags.js
@@ -1,19 +1,67 @@
-import React from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import {StaggeredMotion, spring} from 'react-motion';
 import Nag from './Nag';
 
-const Nags = styled.ul`
+const NagsUl = styled.ul`
   background-color: ${props => props.theme.greyLightest};
   list-style-type: none;
   margin: 0;
   overflow-x: hidden;
   padding: .5rem;
+  will-change: transform;
 `;
 
-export default ({nags}) => (
-  <Nags>
-    {!!nags &&
-      nags.map(n => <Nag key={n.id} nag={n} />)
-    }
-  </Nags>
-)
+const NagsLi = styled.li`
+  cursor: pointer;
+  margin-bottom: .5rem;
+  position: relative;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+`;
+
+const nagsSpring = {stiffness: 150, damping: 16};
+
+export default class Nags extends Component {
+  static propTypes = {
+    nags: PropTypes.arrayOf(PropTypes.object).isRequired
+  };
+
+  getInterpolatedStyles = prevInterpolatedStyles => {
+    const endValue = prevInterpolatedStyles.map((_, i) => {
+      return i === 0 ?
+        {t: spring(0, nagsSpring)} :
+        {t: spring(prevInterpolatedStyles[i - 1].t, nagsSpring)};
+    });
+    return endValue;
+  };
+
+  render() {
+    const nags = this.props.nags;
+
+    return (
+      <StaggeredMotion
+        defaultStyles={Array.from(Array(nags.length)).map(i => ({t: 100}))}
+        styles={this.getInterpolatedStyles}>
+        {interpolatingStyles =>
+          <NagsUl>
+            {interpolatingStyles.map(({t}, i) =>
+              <NagsLi
+                style={{
+                  WebkitTransform: `translateY(${t}px)`,
+                  transform: `translateY(${t}px)`
+                }}
+                key={nags[i].id}
+              >
+                <Nag nag={nags[i]} />
+              </NagsLi>
+            )}
+          </NagsUl>
+        }
+      </StaggeredMotion>
+    )
+  }
+}
