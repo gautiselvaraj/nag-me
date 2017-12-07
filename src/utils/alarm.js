ecad1063ad22ec0840ad86608d9dfedb61df65c9
Gauti <me@gauti.info>
2017-12-08 12:38:04 +1300

    Remove chrome extension fallback

--- src/utils/alarm.js
+++ src/utils/alarm.js
@@ -2,48 +2,17 @@
 
 import { roundedTimestamp } from '../utils/time';
 
-let domTimers = {};
-
-const chromeAlarmAvailable = () =>
-  typeof chrome !== 'undefined' && !!chrome.alarms;
-
 const alarmCreate = (nagId, timestamp) => {
-  const timeout = timestamp - roundedTimestamp();
-  const delayInMinutes = Math.ceil(timeout / 60000);
-
-  if (chromeAlarmAvailable()) {
-    chrome.alarms.create(nagId.toString(), { delayInMinutes });
-  } else {
-    console.log(
-      `No chrome alarm available, using setTimeout and logging for ${nagId}`
-    );
-    if (timeout < 2147483647) {
-      let timer = setTimeout(() => {
-        console.log(`Timeout Fired for ${nagId}`);
-      }, timeout);
-      domTimers[nagId] = timer;
-    }
-  }
+  const delayInMinutes = Math.ceil((timestamp - roundedTimestamp()) / 60000);
+  chrome.alarms.create(nagId.toString(), { delayInMinutes });
 };
 
 const alarmClear = nagId => {
-  if (chromeAlarmAvailable()) {
-    chrome.alarms.clear(nagId);
-  } else {
-    console.log(`No chrome alarm available, clearing timeout for ${nagId}`);
-    clearTimeout(domTimers[nagId]);
-    delete domTimers[nagId];
-  }
+  chrome.alarms.clear(nagId);
 };
 
 export const alarmClearAll = () => {
-  if (chromeAlarmAvailable()) {
-    chrome.alarms.clearAll();
-  } else {
-    console.log('No chrome alarm available, clearing all timeouts');
-    Object.keys(domTimers).forEach(k => clearTimeout(domTimers[k]));
-    domTimers = {};
-  }
+  chrome.alarms.clearAll();
 };
 
 export const clearAndSetAllAlarm = nagList => {
--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,30 +1,13 @@
 /*global chrome*/
 
-const chromeStorageAvailable = () =>
-  typeof chrome !== 'undefined' && !!chrome.storage;
-const localStorageAvailable = () => typeof localStorage !== 'undefined';
-
 export const storageSet = (label, value) => {
-  value = JSON.stringify(value);
-  if (chromeStorageAvailable()) {
-    chrome.storage.sync.set({ [label]: value });
-  } else if (localStorageAvailable) {
-    localStorage.setItem(label, value);
-  }
+  chrome.storage.sync.set({ [label]: JSON.stringify(value) });
 };
 
-export const storageGet = (label, callback) => {
-  if (chromeStorageAvailable()) {
-    return chrome.storage.sync.get([label], item => {
-      const nagsFromStorage = item[label];
-      if (nagsFromStorage) {
-        return callback(JSON.parse(nagsFromStorage));
-      }
-    });
-  } else if (localStorageAvailable) {
-    const nagsFromStorage = localStorage.getItem(label);
+export const storageGet = (label, callback) =>
+  chrome.storage.sync.get([label], item => {
+    const nagsFromStorage = item[label];
     if (nagsFromStorage) {
       return callback(JSON.parse(nagsFromStorage));
     }
-  }
-};
+  });
