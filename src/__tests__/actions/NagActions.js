66edb47035bf41966c151e411ecd511fd1954c25
Gauti <me@gauti.info>
2018-01-30 11:42:09 +1300

    Refactor action tests

--- src/__tests__/actions/NagActions.js
+++ src/__tests__/actions/NagActions.js
@@ -23,31 +23,31 @@ const nagList = [
   {
     id: 1,
     title: 'Test Nag 1',
-    firstNag: 1234567890,
-    nextNag: 1234567890,
+    firstNag: 1517227842000,
+    nextNag: 1517227742000,
     repeats: '1 min',
-    status: 'PAUSED'
+    status: 'LIVE'
   },
   {
     id: 2,
     title: 'Test Nag 2',
-    firstNag: 1234567890,
-    nextNag: 1234567890,
+    firstNag: 1517227862000,
+    nextNag: 1517227862000,
     repeats: '10 min',
     status: 'PAUSED'
   },
   {
     id: 3,
     title: 'Test Nag 3',
-    firstNag: 1234567890,
-    nextNag: 1234567890,
+    firstNag: 1517227862000,
+    nextNag: 1517227862000,
     repeats: '4 hour',
-    status: 'PAUSED'
+    status: 'LIVE'
   }
 ];
 
 describe('Nag Actions', () => {
-  let dispatch, getState;
+  let dispatch, getState, dateNow;
 
   beforeEach(() => {
     dispatch = jest.fn();
@@ -56,27 +56,40 @@ describe('Nag Actions', () => {
     );
   });
 
+  beforeAll(() => {
+    dateNow = window.Date.now;
+    window.Date.now = jest.fn(() => 1517227842390);
+  });
+
+  afterAll(() => {
+    window.Date.now = dateNow;
+  });
+
   describe('nagIndex', () => {
-    it('must call dispatch 3 times and match actions when noreset is false', () => {
+    it('should call dispatch 3 times and match reset and index actions when called with false noreset', () => {
       nagIndex(false)(dispatch, getState);
       expect(dispatch.mock.calls.length).toBe(3);
       expect(dispatch.mock.calls[0][0]).toEqual({
         type: types.NAGS_RESET
       });
-      expect(dispatch.mock.calls[1][0].type).toBe(types.NAG_INDEX);
-      expect(typeof dispatch.mock.calls[1][0].nowTimestamp).toBe('number');
+      expect(dispatch.mock.calls[1][0]).toEqual({
+        type: types.NAG_INDEX,
+        nowTimestamp: 1517227842000
+      });
     });
 
-    it('must call dispatch 2 times and match actions when noreset is true', () => {
+    it('should call dispatch 2 times and match index action when called with true noreset', () => {
       nagIndex(true)(dispatch, getState);
       expect(dispatch.mock.calls.length).toBe(2);
-      expect(dispatch.mock.calls[0][0].type).toBe(types.NAG_INDEX);
-      expect(typeof dispatch.mock.calls[0][0].nowTimestamp).toBe('number');
+      expect(dispatch.mock.calls[0][0]).toEqual({
+        type: types.NAG_INDEX,
+        nowTimestamp: 1517227842000
+      });
     });
   });
 
   describe('nagInit', () => {
-    it('must call dispatch 2 times and match actions when empty nag list is passed', () => {
+    it('should call dispatch 2 times and match init action when called with true empty nag list', () => {
       const nag = { list: [] };
       nagInit(nag)(dispatch);
       expect(dispatch.mock.calls.length).toBe(2);
@@ -86,7 +99,7 @@ describe('Nag Actions', () => {
       });
     });
 
-    it('must call dispatch 2 times and match actions when empty nag list is with arguments', () => {
+    it('should call dispatch 2 times and match init action when called with true nag list', () => {
       const nag = { list: nagList };
       nagInit(nag)(dispatch);
       expect(dispatch.mock.calls.length).toBe(2);
@@ -98,7 +111,7 @@ describe('Nag Actions', () => {
   });
 
   describe('nagNew', () => {
-    it('must call dispatch 2 times and match actions', () => {
+    it('should call dispatch 2 times and match new action', () => {
       nagNew()(dispatch);
       expect(dispatch.mock.calls.length).toBe(2);
       expect(dispatch.mock.calls[0][0]).toEqual({
@@ -108,20 +121,20 @@ describe('Nag Actions', () => {
   });
 
   describe('nagCreate', () => {
-    it('must call dispatch 2 times and match actions', () => {
-      const nag = nagList[0];
+    it('should call dispatch 2 times and match nag title and updated status', () => {
+      const nag = nagList[1];
       nagCreate(nag)(dispatch, getState);
 
       expect(dispatch.mock.calls.length).toBe(2);
       expect(dispatch.mock.calls[0][0].type).toBe(types.NAG_CREATE);
       expect(typeof dispatch.mock.calls[0][0].nag).toBe('object');
-      expect(dispatch.mock.calls[0][0].nag.title).toBe('Test Nag 1');
+      expect(dispatch.mock.calls[0][0].nag.title).toBe('Test Nag 2');
       expect(dispatch.mock.calls[0][0].nag.status).toBe('LIVE');
     });
   });
 
   describe('nagEdit', () => {
-    it('must call dispatch 2 times and match actions', () => {
+    it('should call dispatch 2 times and match edit action', () => {
       const nagId = nagList[0].id;
       nagEdit(nagId)(dispatch);
       expect(dispatch.mock.calls.length).toBe(2);
@@ -133,7 +146,7 @@ describe('Nag Actions', () => {
   });
 
   describe('nagUpdate', () => {
-    it('must call dispatch 2 times and match actions', () => {
+    it('should call dispatch 2 times and match update action', () => {
       const nag = Object.assign({}, nagList[0], { title: 'Updated Title 1' });
       const nagId = nagList[0].id;
       nagUpdate(nagId, nag)(dispatch, getState);
@@ -146,69 +159,79 @@ describe('Nag Actions', () => {
   });
 
   describe('nagStatusUpdate', () => {
-    it('must call dispatch 2 times and match actions', () => {
+    it('should call dispatch 2 times and match status update action and have updated nextNag', () => {
       const nagId = nagList[0].id;
       nagStatusUpdate(nagId)(dispatch, getState);
 
       expect(dispatch.mock.calls.length).toBe(2);
       expect(dispatch.mock.calls[0][0].type).toBe(types.NAG_STATUS_UPDATE);
       expect(typeof dispatch.mock.calls[0][0].nag).toBe('object');
-      expect(dispatch.mock.calls[0][0].nag.title).toBe('Test Nag 1');
+      expect(dispatch.mock.calls[0][0].nag.nextNag).toBe(1517227862000);
     });
   });
 
   describe('nagPause', () => {
-    it('must call dispatch 2 times and match actions', () => {
+    it('should call dispatch 2 times and match pause action', () => {
       const nagId = nagList[0].id;
       nagPause(nagId)(dispatch, getState);
 
       expect(dispatch.mock.calls.length).toBe(2);
-      expect(dispatch.mock.calls[0][0].type).toBe(types.NAG_PAUSE);
-      expect(dispatch.mock.calls[0][0].nagId).toBe(nagId);
+      expect(dispatch.mock.calls[0][0]).toEqual({
+        type: types.NAG_PAUSE,
+        nagId
+      });
     });
   });
 
   describe('nagDelete', () => {
-    it('must call dispatch 2 times and match actions', () => {
+    it('should call dispatch 2 times and match delete action', () => {
       const nagId = nagList[0].id;
       nagDelete(nagId)(dispatch, getState);
 
       expect(dispatch.mock.calls.length).toBe(2);
-      expect(dispatch.mock.calls[0][0].type).toBe(types.NAG_DELETE);
-      expect(dispatch.mock.calls[0][0].nagId).toBe(nagId);
+      expect(dispatch.mock.calls[0][0]).toEqual({
+        type: types.NAG_DELETE,
+        nagId
+      });
     });
   });
 
   describe('nagResume', () => {
-    it('must call dispatch 2 times and match actions', () => {
+    it('should call dispatch 2 times and match resumr action', () => {
       const nagId = nagList[0].id;
       nagResume(nagId)(dispatch, getState);
 
       expect(dispatch.mock.calls.length).toBe(2);
-      expect(dispatch.mock.calls[0][0].type).toBe(types.NAG_RESUME);
-      expect(dispatch.mock.calls[0][0].nagId).toBe(nagId);
+      expect(dispatch.mock.calls[0][0]).toEqual({
+        type: types.NAG_RESUME,
+        nagId
+      });
     });
   });
 
   describe('nagsSearch', () => {
-    it('must call dispatch 2 times and match actions', () => {
+    it('should call dispatch 2 times and match search action', () => {
       const query = 'Search Query';
       nagsSearch(query)(dispatch);
 
       expect(dispatch.mock.calls.length).toBe(2);
-      expect(dispatch.mock.calls[0][0].type).toBe(types.NAGS_SEARCH);
-      expect(dispatch.mock.calls[0][0].query).toBe(query);
+      expect(dispatch.mock.calls[0][0]).toEqual({
+        type: types.NAGS_SEARCH,
+        query
+      });
     });
   });
 
   describe('nagsSort', () => {
-    it('must call dispatch 2 times and match actions', () => {
-      const sortBy = 'A-Z';
+    it('should call dispatch 2 times and match sort action', () => {
+      const sortBy = 'AZ';
       nagsSort(sortBy)(dispatch, getState);
 
       expect(dispatch.mock.calls.length).toBe(2);
-      expect(dispatch.mock.calls[0][0].type).toBe(types.NAGS_SORT);
-      expect(dispatch.mock.calls[0][0].sortBy).toBe(sortBy);
+      expect(dispatch.mock.calls[0][0]).toEqual({
+        type: types.NAGS_SORT,
+        sortBy
+      });
     });
   });
 });
--- src/__tests__/actions/PageActions.js
+++ src/__tests__/actions/PageActions.js
@@ -2,15 +2,17 @@ import { switchPage } from '../../actions/PageActions';
 import * as types from '../../constants/Actions';
 
 describe('Page Actions', () => {
-  it('must call dispatch with expected action', () => {
-    const activePage = 'Index';
-    const dispatch = jest.fn();
-    const getState = jest.fn();
+  describe('switchPage', () => {
+    it('should call dispatch with switch page action', () => {
+      const activePage = 'Index';
+      const dispatch = jest.fn();
+      const getState = jest.fn();
 
-    switchPage(activePage)(dispatch, getState);
-    expect(dispatch).toHaveBeenCalledWith({
-      type: types.SWITCH_PAGE,
-      activePage
+      switchPage(activePage)(dispatch, getState);
+      expect(dispatch).toHaveBeenCalledWith({
+        type: types.SWITCH_PAGE,
+        activePage
+      });
     });
   });
 });
